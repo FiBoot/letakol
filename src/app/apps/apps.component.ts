c68b40f7253d88b36e29027c393be1e86a02b3cf
Fernandez <baptiste.fernandez@ext.euronews.com>
2019-03-05 13:44:17 +0100

    :hammer: :art: apps view + titlecard

--- src/app/apps/apps.component.ts
+++ src/app/apps/apps.component.ts
@@ -1,13 +1,9 @@
 import { Utils } from 'src/app/services/utils/utils.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 class App {
-  constructor(
-    readonly path: string,
-    readonly name: string,
-    readonly description: string = ''
-  ) {}
+  constructor(readonly path: string, readonly name: string, readonly description: string = '') {}
 }
 
 @Component({
@@ -19,19 +15,23 @@ export class AppsComponent {
   apps: Array<App>;
   currentApp: string;
 
-  constructor(activatedRoute: ActivatedRoute) {
+  constructor(private _router: Router, activatedRoute: ActivatedRoute) {
     activatedRoute.url.subscribe(url => {
       const path = Utils.first(activatedRoute.snapshot.children).routeConfig.path;
       this.currentApp = path !== '**' ? path : null;
     });
 
     this.apps = [
-      new App('chat', 'Chat'),
+      new App('keyboard', 'Keyboard', 'Viens tater'),
       new App('codewar', 'CODEWAR', 'Pas comme le corewar mais en JS'),
-      new App('keyboard', 'Keyboard'),
-      new App('memory', 'Memory game (turtle)', 'c::::::::::::::::<'),
-      new App('pet', 'Pet test'),
-      new App('snake', 'Snake', 'c::::::::::::::::<')
+      new App('chat', 'Chat'),
+      new App('snake', 'Snake', 'c::::::::::::::::<'),
+      new App('memory', 'Memory game', 'Une tortue à atteint l\'océan'),
+      new App('pet', 'Pet test')
     ];
+  }
+
+  public click(app: App): void {
+    this._router.navigate(['/', 'apps', app.path]);
   }
 }
