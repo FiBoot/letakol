cd02026386f356adad4942197bba8df54ee75baa
Fernandez <baptiste.fernandez@ext.euronews.com>
2019-04-05 17:00:21 +0200

    :construction: track canvas note height

--- src/app/apps/keyboard/classes/track.class.ts
+++ src/app/apps/keyboard/classes/track.class.ts
@@ -1,4 +1,5 @@
 import { Key } from './key.class';
+import { notes } from './notes';
 import { Player } from './player.class';
 import { UUID } from 'angular2-uuid';
 import { Utils } from 'src/app/services/utils/utils.service';
@@ -6,6 +7,9 @@ import { Utils } from 'src/app/services/utils/utils.service';
 export class RecordedKey {
   constructor(public key: Key, public start: number, public end: number = 0) {}
 }
+
+const NOTE_HEIGHT = 5;
+const NOTE_COLORS = { WHITE: '#fff', BLACK: '#333' };
 
 export class Track extends Player {
   readonly uuid: string = UUID.UUID();
@@ -22,15 +26,22 @@ export class Track extends Player {
   }
 
   public stopCB(): void {
+    // create canvas
     this.canvas = document.createElement('canvas');
-    Object.assign(this.canvas, { width: this.cycle, height: 70 });
+    Object.assign(this.canvas, { width: this.cycle, height: notes.length * NOTE_HEIGHT });
+    Object.assign(this.canvas.style, { border: '1px solid #111' });
     this.ctx = this.canvas.getContext('2d');
+    // draw notes
+    this.ctx.fillStyle = '#aaa';
+    this.ctx.fillRect(0, 0, this.cycle, notes.length * NOTE_HEIGHT);
     this.recordedKeys.forEach(({ key, start, end }) => {
-      const keyHeight = 30; // TODO calc the key height from note
+      const noteHeight = notes.findIndex(note => note === key.note) * NOTE_HEIGHT;
       this.ctx.beginPath();
-      this.ctx.fillStyle = '#333';
-      console.log(`drawing ${key.note.name} from ${start} to ${end - start}`);
-      this.ctx.fillRect(start, keyHeight, end - start, 10);
+      this.ctx.fillStyle = key.sharped ? NOTE_COLORS.BLACK : NOTE_COLORS.WHITE;
+      this.ctx.fillRect(start, noteHeight, end - start, 10);
+      this.ctx.fillStyle = key.sharped ? NOTE_COLORS.WHITE : NOTE_COLORS.BLACK;
+      this.ctx.font = `${NOTE_HEIGHT - 1}px`;
+      this.ctx.fillText(key.note.name, start, noteHeight + NOTE_HEIGHT * 2 - 1);
     });
   }
 
